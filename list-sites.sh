f0cd7cbd23385bc2dc8a40b83279d4204459102d
Marcel Wiget <mwiget@gmail.com>
2022-06-03 14:04:14 +0000

    add instanceid

--- list-sites.sh
+++ list-sites.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 projectPrefix='mwha'
 
-for site in $projectPrefix-tgw-1 ; do
+siteid=1
+for region in eu-north-1 us-west-2; do
+  site="${projectPrefix}-tgw-${siteid}"
   echo "$site:" 
   aws ec2 describe-instances \
-  --region eu-north-1 \
+  --region $region \
   --filters \
     Name=tag:ves-io-creator-id,Values=m.wiget@f5.com \
     Name=tag:ves-io-site-name,Values=$site \
     Name=instance-state-name,Values=running \
-  --query "Reservations[*].Instances[*].[Tags[?Key=='Name'].Value|[0],PublicIpAddress,PrivateIpAddress]" \
+  --query "Reservations[*].Instances[*].[Tags[?Key=='Name'].Value|[0],PublicIpAddress,PrivateIpAddress,InstanceId]" \
   --output=text | sort
   echo ""
-done
-
-for site in $projectPrefix-tgw-2 ; do
-  echo "$site:" 
-  aws ec2 describe-instances \
-  --region us-west-2 \
-  --filters \
-    Name=tag:ves-io-creator-id,Values=m.wiget@f5.com \
-    Name=tag:ves-io-site-name,Values=$site \
-    Name=instance-state-name,Values=running \
-  --query "Reservations[*].Instances[*].[Tags[?Key=='Name'].Value|[0],PublicIpAddress,PrivateIpAddress]" \
-  --output=text | sort
-  echo ""
+  siteid=$((siteid+1))
 done
