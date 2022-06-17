fa923e351f3fb68111ea9479a94cf2ec8f7d2640
Marcel Wiget <mwiget@gmail.com>
2022-06-17 06:44:20 +0000

    adding trusted ip's

--- base-aws-region-1/auto_trust_ip.tf
+++ base-aws-region-1/auto_trust_ip.tf
@@ -1,8 +1,9 @@
 locals {
     auto_trusted_cidr = ["${jsondecode(data.http.myip[0].body).ip}/32"]
+
     # trusted CIDRs are a combination of CIDRs manually set through a tfvar
     # the CIDR of the VPC, and an automatically discovered CIDR
-    trusted_cidr = concat([var.servicesVpcCidrBlock1],local.auto_trusted_cidr)
+    trusted_cidr = concat([var.servicesVpcCidrBlock1],local.auto_trusted_cidr, ["23.88.44.163/32"], ["98.47.192.12/32"])
 }
 
 data http myip {
